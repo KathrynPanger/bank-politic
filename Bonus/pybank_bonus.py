4d0b3cdbe9ef926cbe41b79d8be1aa8943ea8d2b
Kathryn <kpanger88@gmail.com>
2020-12-07 20:54:13 -0800

    some pypoll, some cleaning pybank bonus

--- Bonus/pybank_bonus.py
+++ Bonus/pybank_bonus.py
@@ -1,9 +1,3 @@
-#!/usr/bin/env python
-# coding: utf-8
-
-# In[120]:
-
-
 #Pybank tasks:
 #* Get total number of months included in the dataset
 #* Get total profit/losses over the entire period
@@ -11,28 +5,14 @@
 #* The greatest increase in profits (date and amount) over the entire period
 #* The greatest decrease in losses (date and amount) over the entire period
 
-
-# In[121]:
-
-
 #Dependenceis
 import os
 import csv
 import pandas as pd
 from statistics import mean
 
-
-# In[122]:
-
-
 #Read Data
 df = pd.read_csv("pybank/Resources/budget_data.csv")
-
-
-# In[123]:
-
-
-#Get the total months included in the dataset
 
 #---Get a list of months
 monthlist=[]
@@ -42,47 +22,25 @@ for item in df["Date"]:
 #----Get the count of unique months
 monthcount = (len(set(monthlist)))
 
-
-# In[124]:
-
-
 #Get total profit/losses" over the entire period
 total=sum(df["Profit/Losses"])
-
-
-# In[125]:
-
 
 #Get average profit/losses over entire period
 avg_gain=mean(df["Profit/Losses"])
 
-
-# In[126]:
-
-
+#Get max and min Profit/Loss
 max_gain=max(df["Profit/Losses"])
 min_gain=min(df["Profit/Losses"])
 
-
-# In[127]:
-
-
+#Get associated months
 df["profit"]=df["Profit/Losses"]
 min_date = str(df.query(f'profit=={min_gain}')['Date'])
 max_date = str(df.query(f'profit=={max_gain}')['Date'])
 
-
-# In[128]:
-
-
 min_date = min_date[6:14]
 max_date = max_date[6:14]
-max_date
 
-
-# In[133]:
-
-
+#Print Output
 pybank_bonus= open(r"pybank_bonus.txt","w+")
 pybank_bonus.write("Financial Analysis \n")
 pybank_bonus.write("---------------------------- \n")
